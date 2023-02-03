ef0135121adc9d72f688cf7b96f41142c0f84abb
joyMohanta <joyroy1062@gmail.com>
2023-02-03 22:45:27 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Multiselector from "./components/Multiselector";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text">REACT MULTI SELECTOR !!!</h1>
       <Multiselector></Multiselector>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h5>Here is the React multiselect copyright &#169; Joy Mohanta</h5>
+    </div>
+  );
+};
+
+export default Footer;
